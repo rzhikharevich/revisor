b74d3dc86bd8db408791a2ebfe36ae911e3fa48e
Roman Zhikharevich <rzhikharevich@gmail.com>
2026-03-28 19:59:48 +0000

    Add flake.nix

--- flake.nix
+++ flake.nix
@@ -3,30 +3,43 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    in {
-      packages = forAllSystems (system:
+    in
+    {
+      packages = forAllSystems (
+        system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-        in {
+        in
+        {
           default = pkgs.rustPlatform.buildRustPackage {
             pname = "revisor";
             version = "0.1.0";
             src = ./.;
             cargoLock.lockFile = ./Cargo.lock;
           };
-        });
+        }
+      );
 
-      devShells = forAllSystems (system:
+      devShells = forAllSystems (
+        system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-        in {
+        in
+        {
           default = pkgs.mkShell {
             inputsFrom = [ self.packages.${system}.default ];
           };
-        });
+        }
+      );
     };
 }
